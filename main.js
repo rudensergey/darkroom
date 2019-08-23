012a3d4ca6d10141a8938f411316547f531390b2
Сергей <rudensergey@gmail.com>
2019-08-23 23:58:19 +0300

    add first beta version of switching dark mode

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 let darkModeButton = document.getElementById('activate-button');
+let darkModeLink = document.getElementById('pageBright');
+
+darkModeButton.onclick = () => {
+    if (darkModeLink.id === 'pageBright') {
+        console.log('checking function');
+        darkModeLink.href = './style/index2.css';
+        darkModeLink.id = 'pageDark';
+    } else {
+        console.log('reverse function works');
+        darkModeLink.href = './style/index.css';
+        darkModeLink.id = 'pageBright'
+    }
+}
