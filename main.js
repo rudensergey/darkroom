609ff85f65def4630fb08c6888ec3df7988aa2f0
Сергей <rudensergey@gmail.com>
2019-08-29 20:55:44 +0300

    resolve BIG problem with safari custom property interpretation

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 let darkModeButton = document.getElementById('activate-button');
 let darkModeStatus = false;
+let switchToSafari = document.getElementById('pageBright');
+
+console.log(switchToSafari);
+
+isSafari();
+
+function isSafari() {
+    let browserName = "";
+
+    if(navigator.vendor.match(/apple/i)) {
+        browserName = 'safari/webkit';
+        switchToSafari.href = './style/index-safari.css';
+    } else {
+        browserName = "not safari";
+    }
+
+    console.log(browserName)
+}
 
 darkModeButton.onclick = () => {
     if (darkModeStatus) {
